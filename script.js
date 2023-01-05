30bab22e675945d64f4748ed9b76b41f99bfb3fd
Mitchee Costelo <mcostelo@horizon.csueastbay.edu>
2023-01-05 01:50:48 -0800

    Updated css and js files to include animations

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const button = document.querySelector('.btn-container button');
 const factText = document.querySelector('.container .fact');
 const sourceText = document.querySelector('.container span');
+var splitText  = "";
+var char = 0;
+var timer = 0;
 
 button.addEventListener('click', getFact);
 
@@ -9,4 +12,29 @@ async function getFact() {
     const factObject = await factData.json();
     factText.innerHTML = factObject.text;
     sourceText.innerHTML = factObject.source;
+    factText.textContent = "";
+
+    splitText = factObject.text;
+    for (let i = 0; i < splitText.length; i++) {
+        factText.innerHTML += "<span class='animation'>" + splitText[i] + "</span>";
+    }
+
+    char = 0;
+    timer = setInterval(onTick, 50);
+}
+
+function onTick() {
+    const span = factText.querySelectorAll('.animation')[char];
+    span.classList.add('fade');
+    char++;
+
+    if(char === splitText.length) {
+        complete();
+        return;
+    }
+}
+
+function complete() {
+    clearInterval(timer);
+    timer = null;
 }
